4e4810663318e820e923f32e40fe5b7534754483
Matthew Epshtein <robotmeatsuit@gmail.com>
2023-03-08 08:09:59 -0800

    get target function

--- src/main/kotlin/frc/robot/subsystems/CameraSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/CameraSubsystem.kt
@@ -1,24 +1,36 @@
 package frc.robot.subsystems
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.math.geometry.Translation2d
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.robot.Constants
+import org.photonvision.PhotonCamera
 import org.photonvision.targeting.PhotonPipelineResult
 import org.photonvision.targeting.PhotonTrackedTarget
-import org.photonvision.PhotonCamera
-import frc.robot.Constants
 
 class CameraSubsystem : SubsystemBase() {
     val cam: PhotonCamera = PhotonCamera("CCP BALOON CAMERA")
     public var frame: PhotonPipelineResult = PhotonPipelineResult()
 
-    public fun getTranslationToTarget(target:PhotonTrackedTarget, targetHeightI:Double):Translation2d{
+    public fun getTranslationToTarget(
+            target: PhotonTrackedTarget,
+            targetHeightI: Double
+    ): Translation2d {
         var lengthForward = (targetHeightI - Constants.Camera.camHeightI) / Math.tan(target.pitch)
-        var lengthStafe = lengthForward/Math.tan(target.yaw)
+        var lengthStafe = lengthForward / Math.tan(target.yaw)
 
         return Translation2d(lengthForward, lengthStafe)
     }
 
-    
+    public fun getTarget(bestTarget: Boolean, index: Int): PhotonTrackedTarget? {
+        if (frame.hasTargets()) {
+            return null
+        }
+        if (bestTarget) {
+            return frame.getBestTarget()
+        }
+        return frame.targets[index]
+    }
+
     override fun periodic() {
         // This method will be called once per scheduler run
         frame = cam.getLatestResult()
