03886a3a214a2f8db6facc6af7e333c4d500c386
Matthew Epshtein <robotmeatsuit@gmail.com>
2023-03-07 16:22:56 -0800

    autobalance complete

--- src/main/kotlin/frc/robot/commands/AutoBalanceCommand.kt
+++ src/main/kotlin/frc/robot/commands/AutoBalanceCommand.kt
@@ -1,28 +1,34 @@
 package frc.robot.commands
 
+import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.wpilibj2.command.CommandBase
 import frc.robot.subsystems.DriveSubsystem
 
-class AutoBalanceCommand (drive: DriveSubsystem): CommandBase() {
-    /**
-     * Creates a new AutoBalanceCommand.
-     */
-    
+class AutoBalanceCommand(drive: DriveSubsystem) : CommandBase() {
+    /** Creates a new AutoBalanceCommand. */
+    val drive: DriveSubsystem = DriveSubsystem()
+
+    var gyroAngle: Double = 0.0
     init {
         // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(drive)
+        gyroAngle = drive.getPitch().getDegrees()
     }
 
     // Called when the command is initially scheduled.
-    override fun initialize() { }
+    override fun initialize() {}
 
     // Called every time the scheduler runs while the command is scheduled.
-    override fun execute() { }
+    override fun execute() {
+        gyroAngle = drive.getPitch().getDegrees()
+        drive.drive(Translation2d(Math.sin(gyroAngle) * 1.5, 0.0), 0.0, true, false)
+    }
 
     // Called once the command ends or is interrupted.
-    override fun end(interrupted: Boolean) { }
+    override fun end(interrupted: Boolean) {}
 
     // Returns true when the command should end.
     override fun isFinished(): Boolean {
-        return false
+        return gyroAngle > 5.0
     }
 }
