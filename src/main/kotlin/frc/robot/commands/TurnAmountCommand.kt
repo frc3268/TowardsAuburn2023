2d64a8590900bec3e6c34fe0921755e5e297e1f8
Matthew Epshtein <epshteinmatthew@gmail.com>
2023-03-23 10:39:36 -0700

    more fixes

--- src/main/kotlin/frc/robot/commands/TurnAmountCommand.kt
+++ src/main/kotlin/frc/robot/commands/TurnAmountCommand.kt
@@ -21,7 +21,7 @@ class TurnAmountCommand(target: Double, drive: DriveSubsystem) :
                 // This uses the output
                 // does this shit even work?? idk
                 BiConsumer { output: Double?, setpoint: TrapezoidProfile.State? ->
-                    run { drive.driveArcadeConsumer({ 0.0 }, { output!! }) }
+                    drive.driveArcadeConsumer({ 0.0 }, { output!! })
                 },
                 drive
         ) {
