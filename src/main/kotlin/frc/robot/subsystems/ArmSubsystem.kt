b22364f00d550d921ac53c6559f9717c23b2fa67
Matthew Epshtein <epshteinmatthew@gmail.com>
2023-03-23 17:17:24 -0700

    fixed issues

--- src/main/kotlin/frc/robot/subsystems/ArmSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/ArmSubsystem.kt
@@ -1,12 +1,15 @@
 package frc.robot.subsystems
 
+import com.revrobotics.CANSparkMax
+import com.revrobotics.CANSparkMaxLowLevel.MotorType
+import com.revrobotics.RelativeEncoder
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 import frc.robot.Constants
 import frc.robot.lib.units.*
 
 class ArmSubsystem() : SubsystemBase() {
-    val motor: CANSparkMax = CANSparkMax(ArmConsts.motorPort, MotorType.kBrushless)
+    val motor: CANSparkMax = CANSparkMax(0, MotorType.kBrushless)
     val encoder: RelativeEncoder = motor.getEncoder()
 
     init {
